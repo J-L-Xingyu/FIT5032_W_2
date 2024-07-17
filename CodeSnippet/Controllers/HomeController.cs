f11d0903eb2793000b7abb1efc06f2faf2a64bd2
J-L-Xingyu <littlemonsterlxy@icloud.com>
2024-07-17 22:09:50 +0800

    add Studio 2

---
+++ CodeSnippet/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using CodeSnippet.Exercise;
+using CodeSnippet.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeSnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Hello hello = new Hello();
+            //ViewBag.Message = "Your application description page.";
+            ViewBag.Message = hello.GetHello();
+            ExampleDictionary ed = new ExampleDictionary();
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
